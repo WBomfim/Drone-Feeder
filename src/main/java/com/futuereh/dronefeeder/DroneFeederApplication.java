c37c6a2b5e3dde07457980227781da576f47a861
WBomfim <willianbomfim@hotmail.com>
2023-04-03 22:23:39 -0300

    build: Contruído as configurações iniciais para o desenvolvimento do projeto

---
+++ src/main/java/com/futuereh/dronefeeder/DroneFeederApplication.java
@@ -0,0 +1,17 @@
+package com.futuereh.dronefeeder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Class to start the application.
+ * 
+ */
+@SpringBootApplication
+public class DroneFeederApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DroneFeederApplication.class, args);
+  }
+
+}
