ec046330229a6636eed6e52838e9dfd792d9b5af
WBomfim <willianbomfim@hotmail.com>
2023-04-11 17:27:04 -0300

    test: Criado um script SQL para iniciar o banco de teste com informações pré definidas para os testes

---
+++ src/test/resources/data/init-sql-test.sql
@@ -0,0 +1,5 @@
+INSERT INTO clients (`id`, `name`, `password`) VALUES (1, 'client1', 'password1');
+INSERT INTO clients (`id`, `name`, `password`) VALUES (2, 'client2', 'password2');
+
+INSERT INTO drones (`id`, `status`) VALUES (1, 'AVAILABLE');
+INSERT INTO drones (`id`, `status`) VALUES (2, 'AVAILABLE');
