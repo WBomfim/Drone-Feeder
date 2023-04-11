ed27d23341ac6611c1483a84645ea8666661096e
WBomfim <willianbomfim@hotmail.com>
2023-04-11 17:27:50 -0300

    feat: Implementado testes para a rota de cliente

---
+++ src/test/java/com/futuereh/dronefeeder/integrations/ClientTest.java
@@ -0,0 +1,48 @@
+package com.futuereh.dronefeeder.integrations;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.futuereh.dronefeeder.persistence.models.Client;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+/**
+ * Class to test the client.
+ * @
+ */
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class ClientTest {
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @Test
+  @Order(1)
+  public void testClient() throws Exception {
+    Client client = new Client();
+    client.setName("Test");
+    client.setPassword("ClientTest123");
+
+    mockMvc
+      .perform(
+        post("/client")
+          .contentType(MediaType.APPLICATION_JSON)
+          .content(new ObjectMapper().writeValueAsString(client))
+      )
+        .andExpect(status().isCreated())
+        .andExpect(jsonPath("$.message").value("Client saved successfully"));
+  }
+  
+}
